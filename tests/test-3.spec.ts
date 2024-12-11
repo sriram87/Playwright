b4c92aaf696a361b5438f7f76dcc05a74a8a453e
Sriram87 <sriram.ramanujam1@gmail.com>
2024-12-11 16:57:44 +0800

    Updated_Changes

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -12,20 +12,11 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Postcode').first().fill('35600');
   await page.getByPlaceholder('Select country').fill('India');
   await page.getByText('India - IN').click();
-  await page.getByPlaceholder('eg. 0.1kg').click();
   await page.getByPlaceholder('eg. 0.1kg').fill('1');
   await page.getByText('Calculate').click();
 
   await page.getByText('Service Type International Air Parcel').scrollIntoViewIfNeeded();
 
-  expect(await page.locator("//dd[normalize-space()='International Air Parcel']")).toHaveText('International Air Parcel')
-
-  // await page.locator('div').filter({ hasText: /^Weight \(kg\)Max Weight is 30 kg$/ }).first().click();
-  // await page.getByText('Calculate').click();
-
-  // await page.locator('#contentBody div').filter({ hasText: 'Get your domestic or' }).nth(2).click();
-  // await page.getByText('Calculate').click();
-
-
+  expect(await page.locator("//dd[normalize-space()='International Air Parcel']")).toHaveText('International Air Parcel');
 
 });
