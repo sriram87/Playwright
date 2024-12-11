9c9748294935cb664215dec9e971ce6de9fb92e9
sriram87 <58701386+sriram87@users.noreply.github.com>
2024-12-11 17:05:46 +0800

    Update test-3.spec.ts

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -4,20 +4,15 @@ test('test', async ({ page }) => {
   await page.goto('https://www.pos.com.my/send/ratecalculator');
 
   await page.waitForTimeout(3000)
-
-  let pincode= await page.getByPlaceholder('Postcode').first();
-  pincode.scrollIntoViewIfNeeded();
   await page.getByPlaceholder('Postcode').first().fill('35600');
   await page.getByPlaceholder('Select country').fill('India');
   await page.getByText('India - IN').click();
-  await page.getByPlaceholder('eg. 0.1kg').click();
   await page.getByPlaceholder('eg. 0.1kg').fill('1');
   await page.getByText('Calculate').click();
 
   await page.getByText('Service Type International Air Parcel').scrollIntoViewIfNeeded();
 
-  expect(await page.locator("//dd[normalize-space()='International Air Parcel']")).toHaveText('International Air Parcel')
-
-
+  expect(page.locator("//dd[normalize-space()='International Air Parcel']")).toHaveText('International Air Parcel');
 
 });
+
